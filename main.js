d605a3b9fd07e3fa7d14fbaa2e1a047a3dd01632
phizzixx <77556706+phizzixx@users.noreply.github.com>
2021-01-22 13:56:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -139,11 +139,25 @@ client.on('message', message =>{
         } else {
             message.reply("You're already free!")
         }
+    } else if (mainCommand == 'finalroulette'){
+        if((message.member.roles.cache.find(r => r.name === "Roulette")) && !(message.member.roles.cache.find(r => r.name === "Horny"))){
+            barrels--;
+            if (barrels == 0){
+                message.reply('You have been shot and killed. See ya later!');
+                message.member.kick(myRole);
+                message.channel.send('*reloading and spinning the revolver* <a:loading:802093362005803049>');
+                barrels = getRandomInt(6);
+            } else {
+                message.channel.send('Phew, the barrel chamber was empty!');
+            }
+        } else {
+            message.reply("You have to lose to %roulette first!");
+        }
     } else if (mainCommand == 'pet') {
         let arr = ['*happy robot sounds*', '*excited beeping*', '*energetic static sound*', '*calculating my love for you*', '*robotic humming*', '*blue screen of happiness*', '*spins in place*', '*pulls you in for robot hug*', '*systems overloaded from happiness*', '*robotic barking*', '*meow*', '*01101001 01101100 01111001*', '*woof*', '*jumps up and down*', '*beep boop*']
         message.channel.send(arr[getRandomInt(15)-1] + ' >w< <:peepoShy:782174763115610124>');
     } else if (mainCommand == 'help') {
-        message.reply('\n**%uwu** - uwu-fys messages that you reply to\n**%monke** - monke\n**%jail [user]** - mutes a user and puts them in jail\n**%free [user]** - frees a user from jail\n**%roulette** - shoots from a revolver with 1 bullet in the 6 chamber barrel\n**%escape** - answer the question to free yourself after being shot\n**%override** - remove a current escape attempt\n**%pet** - pet the bot');
+        message.reply('\n**%uwu** - uwu-fys messages that you reply to\n**%monke** - monke\n**%jail [user]** - mutes a user and puts them in jail\n**%free [user]** - frees a user from jail\n**%roulette** - shoots from a revolver with 1 bullet in the 6 chamber barrel\n**%escape** - answer the question to free yourself after being shot\n**%override** - remove a current escape attempt\n**%pet** - pet the bot\n**%finalroulette** - proceed with caution. if you lose to this, you will be kicked');
     }
 })
 
