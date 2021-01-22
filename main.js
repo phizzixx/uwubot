5b927d10bf78af8537508836c1f6937d1ae64d3e
phizzixx <77556706+phizzixx@users.noreply.github.com>
2021-01-22 03:21:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ client.on('message', message =>{
                 message.member.roles.add(myRole);
                 message.member.roles.add(myRole2);
                 message.reply('You have been shot and put in jail!');
-                message.channel.send('*reloading and spinning the revolver*');
+                message.channel.send('*reloading and spinning the revolver* :loading:');
                 barrels = getRandomInt(6);
             } else {
                 message.channel.send('Phew, the barrel chamber was empty!');
