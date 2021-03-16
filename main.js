269e2b63edc66facdfb2048792551ff571eec4fa
phizzixx <77556706+phizzixx@users.noreply.github.com>
2021-03-15 20:32:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -216,7 +216,7 @@ client.on('message', message =>{
             if(scoreDict.has(message.author.id)){
                 scoreDict.set(message.author.id, scoreDict.get(message.author.id)-1);
                 if(parseFloat(scoreDict.get(message.author.id)) < parseFloat(-7)){
-                    message.reply("You have been placed in jail for repeated missing! You can escape wioth %escape")
+                    message.reply("You have been placed in jail for repeated missing! You can escape with %escape")
                     myRole = message.guild.roles.cache.find(role => role.name === "Roulette");
                     myRole2 = message.guild.roles.cache.find(role => role.name === "Muted");
                     message.member.roles.add(myRole);
