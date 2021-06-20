3cdf1332d5d67c01027147e24c8f8a8a23f4440a
phizzixx <77556706+phizzixx@users.noreply.github.com>
2021-06-20 17:39:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -680,7 +680,7 @@ client.on('message', message =>{
                     let arr = ["BANNED", "Beta", "Forest", "Insomniacs", "Phizz Simp", "Froge", "Minecwaft", "Epic Gamers", "Switch"];
                     arr.forEach(el => {
                         user.roles.add(message.guild.roles.cache.find(role => role.name === el);
-                    }
+                    });
                     message.reply("Roles have been added!");
                 } else {
                     message.reply("You're not a sheriff!");
