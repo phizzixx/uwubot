b1d2a76a5fe9298f7fe9813d01bc7bd78acd4539
phizzixx <77556706+phizzixx@users.noreply.github.com>
2021-07-26 20:55:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -756,9 +756,9 @@ function image(message){
         $ = cheerio.load(responseBody);
         var links = $(".image a.link");
         var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-        if(!urls.length){
+        /*if(!urls.length){
             return;
-        }
+        }*/
         message.channel.send('https://cdn.vox-cdn.com/thumbor/oFGuiTBJrDBPPz3G_C8ewGzq-dE=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/assets/4867012/Macaca_nigra_self-portrait__rotated_and_cropped_.jpg');
     });
 }
